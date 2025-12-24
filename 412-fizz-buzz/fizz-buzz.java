78d4bd04cde5701e35cf1041a90ccad3e9b1daff
BHARATHRAJ R <bharathraj2400@gmail.com>
2025-12-24 10:28:36 +0530

    Time: 1 ms (99.87%) | Memory: 47.1 MB (9.02%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -11,3 +11,12 @@ class Solution {
         return list;
     }
 }
+
+/*class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> l = new ArrayList<>();
+        for (int i = 1; i <= n; i++)
+            l.add(i % 15 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : "" + i);
+        return l;
+    }
+}*/
