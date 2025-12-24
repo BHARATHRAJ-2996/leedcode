864e9e4a236f95ddb0983deccd15b038560fcf0d
BHARATHRAJ R <bharathraj2400@gmail.com>
2025-12-24 10:27:22 +0530

    Time: 1 ms (99.87%) | Memory: 47 MB (9.02%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,8 +1,13 @@
+import java.util.*;
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> l = new ArrayList<>();
-        for (int i = 1; i <= n; i++)
-            l.add(i % 15 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : "" + i);
-        return l;
+        List<String> list= new ArrayList<>();
+        for(int i =1 ;i<=n;i++){
+        if(i%15==0) list.add("FizzBuzz");
+       else if(i%3==0) list.add("Fizz");
+         else if(i%5==0) list.add("Buzz");
+         else list.add(String.valueOf(i));
+        }
+        return list;
     }
 }
